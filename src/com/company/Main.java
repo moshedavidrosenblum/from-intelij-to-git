3dc435ace5463e0b44095ac4a4c6306c712666c7
moshedavidrosenblum <mosherosenblum1@gmail.com>
2020-03-30 16:21:28 +0300

    you getting beter and beter

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("moshe you are the best");
+        Scanner scanner=new Scanner(System.in);
+        String user=scanner.nextLine();
+        System.out.println(user);
+
     }
 
 }
